05bc999c2c67e6d90e231f889b74a70bc1bf1a9d
Lee Gathercole <lee@fathomatics.com>
2015-04-29 15:38:32 +0100

    Purge

--- roles/activedirectory/files/install_ad_forest.ps1
+++
@@ -1,2 +0,0 @@
-Import-Module ADDSDeployment Install-ADDSForest -DomainName "{{rootdomain}}" -DomainNetbiosName "{{netbios}}" -safemodeadministratorpassword (convertto-securestring {{windows_ssh_pass}} -asplaintext -force) -Force:$true
-
